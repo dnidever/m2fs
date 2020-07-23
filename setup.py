b5e2847bc1c68ae91e9fb17f91ac0010543122d1
dnidever <dnidever@noao.edu>
2020-07-23 11:26:57 -0700

    Added setup.py and moved scripts to bin/ and regular code to package directory m2fs/

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='m2fs',
+      version='1.0',
+      description='M2FS Data Reduction Pipeline',
+      author='Matt Walker, David Nidever',
+      author_email='mgwalker21@gmail.com',
+      url='https://github.com/dnidever/m2fs',
+      packages=['m2fs'],
+      scripts=['bin/m2fs_zero'],
+      requires=['numpy','astropy','scipy']
+)
