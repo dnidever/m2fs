559037b154410934621a98577052768f02918057
David Nidever <dnidever@noao.edu>
2020-07-24 21:43:06 -0700

    Updates to m2fs_apertures

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(name='m2fs',
       author_email='mgwalker21@gmail.com',
       url='https://github.com/dnidever/m2fs',
       packages=['m2fs'],
-      scripts=['bin/m2fs_zero','bin/m2fs_dark','bin/m2fs_reduce'],
+      scripts=['bin/m2fs_zero','bin/m2fs_dark','bin/m2fs_reduce','bin/m2fs_apertures'],
       requires=['numpy','astropy','scipy','specutils','ccdproc']
 )
